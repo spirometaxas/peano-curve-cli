75a311bd29969642546980c20021f6ac78c6ac3e
Spiro Metaxas <spirometaxas@gmail.com>
2022-12-28 16:44:50 -0500

    Version 1.1.0

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,105 @@
 #!/usr/bin/env node
 const peano = require('./index.js');
 
-const printUsage = function() {
-    console.log('\nUsage:\n' + '  $ peano-curve-cli <n>\n');
+const printUsage = function(showIntro) {
+    if (showIntro) {
+        console.log(peano.create(2));
+        console.log(' Print the Peano Curve to the console!');
+    }
+    console.log('\n' + 
+                ' Usage:\n' + 
+                '   $ peano-curve-cli <n>\n' + 
+                '\n' + 
+                '   <n> is the recursive step, a number greater than or equal to 1\n' + 
+                '\n' +
+                ' Options:\n' + 
+                '   --inverse, -i      Draw the inverse Peano Curve\n' + 
+                '   --line=<line>      Draw using a specific line type: [bold|double|standard]\n' + 
+                '   --rotate=<rotate>  Rotate the Peano Curve: [left|right|flip|standard]\n');
+}
+
+const getFlags = function(params) {
+    let flags = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (params[i].startsWith('-')) {
+                flags.push(params[i]);
+            }
+        }
+    }
+    return flags;
+}
+
+const getValues = function(params) {
+    let values = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (!params[i].startsWith('-')) {
+                values.push(params[i]);
+            }
+        }
+    }
+    return values;
+}
+
+const drawInverse = function(flags) {
+    for (let i = 0; i < flags.length; i++) {
+        if (flags[i] && (flags[i].toLowerCase() === '-i' || flags[i].toLowerCase() === '--inverse')) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const getLine = function(flags) {
+    for (let i = 0; i < flags.length; i++) {
+        if (flags[i] && flags[i].toLowerCase().startsWith('--line=')) {
+            const line = flags[i].substring(7);
+            if (line) {
+                if (line.toLowerCase() === 'bold' || line.toLowerCase() === 'double' || line.toLowerCase() === 'standard') {
+                    return line.toLowerCase();
+                } else {
+                    console.log('\n Warning: Please provide a supported line type: [bold|double|standard]');
+                }
+            } else {
+                console.log('\n Warning: Please provide a supported line type: [bold|double|standard]');
+            }
+        }
+    }
+    return undefined;
+}
+
+const getRotation = function(flags) {
+    for (let i = 0; i < flags.length; i++) {
+        if (flags[i] && flags[i].toLowerCase().startsWith('--rotate=')) {
+            const line = flags[i].substring(9);
+            if (line) {
+                if (line.toLowerCase() === 'left' || line.toLowerCase() === 'right' || line.toLowerCase() === 'flip' || line.toLowerCase() === 'standard') {
+                    return line.toLowerCase();
+                } else {
+                    console.log('\n Warning: Please provide a supported rotation type: [left|right|flip|standard]');
+                }
+            } else {
+                console.log('\n Warning: Please provide a supported rotation type: [left|right|flip|standard]');
+            }
+        }
+    }
+    return undefined;
 }
 
 if (process.argv.length > 2) {
     const params = process.argv.slice(2);
+    const values = getValues(params);
+    const flags = getFlags(params);
     if (params[0] && !isNaN(params[0]) && parseInt(params[0]) >= 1) {
         var n = parseInt(params[0]);
         if (n !== undefined) {
-            console.log(peano.create(n));
+            console.log(peano.create(n, { inverse: drawInverse(flags), rotate: getRotation(flags), line: getLine(flags) }));
         }
     } else {
-        console.log('\n<n> should be a number greater than or equal to 1');
-        printUsage();
+        console.log('\n <n> should be a number greater than or equal to 1');
+        printUsage(false);
     }
 } else {
-    printUsage();
+    printUsage(true);
 }
--- src/index.js
+++ src/index.js
@@ -12,6 +12,60 @@ const getHeight = function(n) {
   return Math.pow(3, n);
 }
 
+const LINES = {
+  '─': {
+    STANDARD: '─',
+    BOLD: '━',
+    DOUBLE: '═',
+  },
+  '│': {
+    STANDARD: '│',
+    BOLD: '┃',
+    DOUBLE: '║',
+  },
+  '┌': {
+    STANDARD: '┌',
+    BOLD: '┏',
+    DOUBLE: '╔',
+  },
+  '┐': {
+    STANDARD: '┐',
+    BOLD: '┓',
+    DOUBLE: '╗',
+  },
+  '┘': {
+    STANDARD: '┘',
+    BOLD: '┛',
+    DOUBLE: '╝',
+  },
+  '└': {
+    STANDARD: '└',
+    BOLD: '┗',
+    DOUBLE: '╚',
+  },
+};
+
+const getLine = function(lineId, lineType) {
+  if (LINES[lineId] !== undefined && lineType !== undefined) {
+    return LINES[lineId][lineType.toUpperCase()];
+  } else if (LINES[lineId] !== undefined) {
+    return LINES[lineId].STANDARD;
+  } else {
+    return ' ';
+  }
+}
+
+const getLineType = function(line) {
+  if (line !== undefined && (line.toLowerCase() === 'standard' || line.toLowerCase() === 'double' || line.toLowerCase() === 'bold')) {
+    return line.toLowerCase();
+  }
+  return 'standard';
+}
+
+const isValidRotation = function(rotation) {
+  return rotation !== undefined && (rotation.toLowerCase() === 'left' || rotation.toLowerCase() === 'right' || rotation.toLowerCase() === 'flip' || rotation.toLowerCase() === 'standard');
+}
+
 const createBoard = function(w, h) {
   let board = [];
   for (let i = 0; i < h; i++) {
@@ -76,57 +130,164 @@ const drawCurve = function(board, pos, standard, params) {
   }
 }
 
-const peano = function(n, board, pos, standard, params) {
-  if (n <= 1) {
-    drawCurve(board, pos, standard, params);
+const drawCurveLR = function(board, pos, standard, params) {
+  if (standard) {
+    board[pos.y + 2][pos.x + 1] = '─';
+    board[pos.y + 2][pos.x + 2] = '─';
+    board[pos.y + 2][pos.x + 3] = '─';
+    board[pos.y + 2][pos.x + 4] = '┐';
+    board[pos.y + 1][pos.x + 4] = '┘';
+    board[pos.y + 1][pos.x + 3] = '─';
+    board[pos.y + 1][pos.x + 2] = '─';
+    board[pos.y + 1][pos.x + 1] = '─';
+    board[pos.y + 1][pos.x] = '┌';
+    board[pos.y][pos.x] = '└';
+    board[pos.y][pos.x + 1] = '─';
+    board[pos.y][pos.x + 2] = '─';
+    board[pos.y][pos.x + 3] = '─';
+    board[pos.y][pos.x + 4] = '─';
+
+    if (params && params.end1 === 'left') {
+      board[pos.y + 2][pos.x] = '─';
+    }
+    if (params && params.end1 === 'up') {
+      board[pos.y + 2][pos.x] = '└';
+    }
+    if (params && params.end2 === 'right') {
+      board[pos.y][pos.x + 4] = '─';
+      board[pos.y][pos.x + 5] = '─';
+    }
+    if (params && params.end2 === 'down') {
+      board[pos.y][pos.x + 4] = '┐';
+    }
   } else {
-    if (standard) {
-      peano(n - 1, board, { x: pos.x, y: pos.y }, true, { end1: params.end1, end2: 'up' });
-      peano(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, false, { end1: 'up', end2: 'down' });
-      peano(n - 1, board, { x: pos.x, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'down', end2: 'right' });
+    board[pos.y][pos.x + 1] = '─';
+    board[pos.y][pos.x + 2] = '─';
+    board[pos.y][pos.x + 3] = '─';
+    board[pos.y][pos.x + 4] = '┘';
+    board[pos.y + 1][pos.x + 4] = '┐';
+    board[pos.y + 1][pos.x + 3] = '─';
+    board[pos.y + 1][pos.x + 2] = '─';
+    board[pos.y + 1][pos.x + 1] = '─';
+    board[pos.y + 1][pos.x] = '└';
+    board[pos.y + 2][pos.x] = '┌';
+    board[pos.y + 2][pos.x + 1] = '─';
+    board[pos.y + 2][pos.x + 2] = '─';
+    board[pos.y + 2][pos.x + 3] = '─';
+    board[pos.y + 2][pos.x + 4] = '─';
 
-      peano(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: 'left', end2: 'down' });
-      peano(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, true, { end1: 'down', end2: 'up' });
-      peano(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, false, { end1: 'up', end2: 'right' });
-
-      peano(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y }, true, { end1: 'left', end2: 'up' });
-      peano(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + getHeight(n - 1) }, false, { end1: 'up', end2: 'down' });
-      peano(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'down', end2: params.end2 });
-    } else {
-      peano(n - 1, board, { x: pos.x, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: params.end1, end2: 'down' });
-      peano(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, true, { end1: 'down', end2: 'up' });
-      peano(n - 1, board, { x: pos.x, y: pos.y }, false, { end1: 'up', end2: 'right' });
-
-      peano(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, true, { end1: 'left', end2: 'up' });
-      peano(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, false, { end1: 'up', end2: 'down' });
-      peano(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'down', end2: 'right' });
-
-      peano(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: 'left', end2: 'down' });
-      peano(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + getHeight(n - 1) }, true, { end1: 'down', end2: 'up' });
-      peano(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y }, false, { end1: 'up', end2: params.end2 });
+    if (params && params.end1 === 'left') {
+      board[pos.y][pos.x] = '─';
+    }
+    if (params && params.end1 === 'down') {
+      board[pos.y][pos.x] = '┌';
+    }
+    if (params && params.end2 === 'right') {
+      board[pos.y + 2][pos.x + 4] = '─';
+      board[pos.y + 2][pos.x + 5] = '─';
+    }
+    if (params && params.end2 === 'up') {
+      board[pos.y + 2][pos.x + 4] = '┘';
     }
   }
 }
 
-const draw = function(board) {
+const peanoSF = function(n, board, pos, standard, params) {
+  if (n <= 1) {
+    drawCurve(board, pos, standard, params);
+  } else {
+    if (standard) {
+      peanoSF(n - 1, board, { x: pos.x, y: pos.y }, true, { end1: params.end1, end2: 'up' });
+      peanoSF(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, false, { end1: 'up', end2: 'down' });
+      peanoSF(n - 1, board, { x: pos.x, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'down', end2: 'right' });
+
+      peanoSF(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: 'left', end2: 'down' });
+      peanoSF(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, true, { end1: 'down', end2: 'up' });
+      peanoSF(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, false, { end1: 'up', end2: 'right' });
+
+      peanoSF(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y }, true, { end1: 'left', end2: 'up' });
+      peanoSF(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + getHeight(n - 1) }, false, { end1: 'up', end2: 'down' });
+      peanoSF(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'down', end2: params.end2 });
+    } else {
+      peanoSF(n - 1, board, { x: pos.x, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: params.end1, end2: 'down' });
+      peanoSF(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, true, { end1: 'down', end2: 'up' });
+      peanoSF(n - 1, board, { x: pos.x, y: pos.y }, false, { end1: 'up', end2: 'right' });
+
+      peanoSF(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, true, { end1: 'left', end2: 'up' });
+      peanoSF(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, false, { end1: 'up', end2: 'down' });
+      peanoSF(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'down', end2: 'right' });
+
+      peanoSF(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: 'left', end2: 'down' });
+      peanoSF(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + getHeight(n - 1) }, true, { end1: 'down', end2: 'up' });
+      peanoSF(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y }, false, { end1: 'up', end2: params.end2 });
+    }
+  }
+}
+
+const peanoLR = function(n, board, pos, standard, params) {
+  if (n <= 1) {
+    drawCurveLR(board, pos, standard, params);
+  } else {
+    if (standard) {
+      peanoLR(n - 1, board, { x: pos.x, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: params.end1, end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: 'left', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'left', end2: 'down' });
+
+      peanoLR(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + getHeight(n - 1) }, false, { end1: 'left', end2: 'up' });
+      peanoLR(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, true, { end1: 'left', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, false, { end1: 'down', end2: 'right' });
+
+      peanoLR(n - 1, board, { x: pos.x, y: pos.y }, true, { end1: 'up', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, false, { end1: 'left', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y }, true, { end1: 'left', end2: params.end2 });
+    } else {
+      peanoLR(n - 1, board, { x: pos.x, y: pos.y }, false, { end1: params.end1, end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y }, true, { end1: 'left', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y }, false, { end1: 'left', end2: 'up' });
+
+      peanoLR(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + getHeight(n - 1) }, true, { end1: 'left', end2: 'down' });
+      peanoLR(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + getHeight(n - 1) }, false, { end1: 'left', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x, y: pos.y + getHeight(n - 1) }, true, { end1: 'up', end2: 'right' });
+
+      peanoLR(n - 1, board, { x: pos.x, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: 'down', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + getWidth(n - 1) + 1, y: pos.y + (2 * getHeight(n - 1)) }, true, { end1: 'left', end2: 'right' });
+      peanoLR(n - 1, board, { x: pos.x + (2 * getWidth(n - 1)) + 2, y: pos.y + (2 * getHeight(n - 1)) }, false, { end1: 'left', end2: params.end2 });
+    }
+  }
+}
+
+const peano = function(n, board, rotate, inverse) {
+  if (rotate === 'left' || rotate === 'right') {
+    peanoLR(n, board, { x: 0, y: 0 }, !inverse, { end1: 'left', end2: 'right' });
+  } else {
+    peanoSF(n, board, { x: 0, y: 0 }, !inverse, inverse ? { end1: 'up', end2: 'down' } : { end1: 'down', end2: 'up' });
+  }
+}
+
+const draw = function(board, lineType) {
   var result = '\n ';
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      result += board[board.length - i - 1][j];
+      result += getLine(board[board.length - i - 1][j], lineType);
     }
     result += '\n ';
   }
   return result;
 }
 
-const create = function(n) {
+const create = function(n, config) {
   if (n === undefined || isNaN(n) || n < 1) {
     return '';
   }
 
+  const inverse = config !== undefined && config.inverse === true;
+  const rotate = config !== undefined && isValidRotation(config.rotate) ? config.rotate.toLowerCase() : 'standard';
+  const lineType = config !== undefined ? getLineType(config.line) : undefined;
+
   const board = createBoard(getWidth(n), getHeight(n));
-  peano(n, board, { x: 0, y: 0 }, true, { end1: 'down', end2: 'up' });
-  return draw(board);
+  peano(n, board, rotate, inverse);
+  
+  return draw(board, lineType);
 }
 
 module.exports = {
